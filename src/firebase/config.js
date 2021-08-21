1889e7b7d7fb067c323a2a1c8ed15983ff8d15c3
Lennox Gilbert <lennoxgilbert@yahoo.com>
2021-08-21 10:03:19 -0400

    added date, title and location

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,12 +1,18 @@
-import firebase from 'firebase'
-import '@firebase/auth';
-import '@firebase/database';
+import firebase from "firebase";
+import "@firebase/auth";
+import "@firebase/database";
 
 const firebaseConfig = {
-    apiKey: "Copy Your config from the firebase console"
+  apiKey: "AIzaSyD6_EVsnfgvbCWmGVCbsaanAQy_zGEHl_o",
+  authDomain: "firebasics-21d81.firebaseapp.com",
+  projectId: "firebasics-21d81",
+  storageBucket: "firebasics-21d81.appspot.com",
+  messagingSenderId: "546546656768",
+  appId: "1:546546656768:web:c2f58582299baa26595ae5",
+  measurementId: "G-MZQD29QTV2",
 };
 
 if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-} 
+  firebase.initializeApp(firebaseConfig);
+}
 export { firebase };
--- src/screens/HomeScreen/styles.js
+++ src/screens/HomeScreen/styles.js
@@ -1,82 +1,96 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center'
-    },
-    formContainer: {
-        flexDirection: 'row',
-        height: 80,
-        marginTop: 40,
-        marginBottom: 20,
-        flex: 1,
-        paddingTop: 10,
-        paddingBottom: 10,
-        paddingLeft: 30,
-        paddingRight: 30,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    input: {
-        height: 48,
-        borderRadius: 5,
-        overflow: 'hidden',
-        backgroundColor: 'white',
-        paddingLeft: 16,
-        flex: 1,
-        marginRight: 5
-    },
-    button: {
-        height: 47,
-        borderRadius: 5,
-        backgroundColor: '#788eec',
-        width: 80,
-        alignItems: "center",
-        justifyContent: 'center'
-    },
-    buttonText: {
-        color: 'white',
-        fontSize: 16
-    },
-    listContainer: {
-        marginTop: 20,
-        padding: 20,
-    },
-    entityContainer: {
-        marginTop: 16,
-        borderBottomColor: '#cccccc',
-        borderBottomWidth: 1,
-        paddingBottom: 16
-    },
-    entityText: {
-        fontSize: 20,
-        color: '#333333'
-    },
-    fab: {
-        position: 'absolute',
-        margin: 16,
-        right: 0,
-        top: 0,
-    },
-    containerStyle: {
-        flex: 1,
-    },
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+  formContainer: {
+    flexDirection: "row",
+    height: 80,
+    marginTop: 40,
+    marginBottom: 20,
+    flex: 1,
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 30,
+    paddingRight: 30,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  input: {
+    height: 48,
+    borderRadius: 5,
+    overflow: "hidden",
+    backgroundColor: "white",
+    paddingLeft: 16,
+    flex: 1,
+    marginRight: 5,
+  },
+  button: {
+    height: 47,
+    borderRadius: 5,
+    backgroundColor: "#788eec",
+    width: 80,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+  },
+  listContainer: {
+    marginTop: 20,
+    padding: 20,
+  },
+  entityContainer: {
+    marginTop: 16,
+    borderBottomColor: "#cccccc",
+    borderBottomWidth: 1,
+    paddingBottom: 16,
+  },
+  entityText: {
+    fontSize: 20,
+    color: "#333333",
+  },
+  date: {
+    display: "flex",
+    gap: 20 + "em",
+  },
+  spaceBetween: {
+    display: "flex",
+    justifyContent: "space-between",
+    flexDirection: "row",
+  },
 
-    scrollViewStyle: {
-        flex: 1,
+  cardBottom: {
+    display: "flex",
+    justifyContent: "spaceBetween",
+    flex: 1,
+  },
+  fab: {
+    position: "absolute",
+    margin: 16,
+    right: 0,
+    top: 0,
+  },
+  containerStyle: {
+    flex: 1,
+  },
 
-        padding: 15,
+  scrollViewStyle: {
+    flex: 1,
 
-        justifyContent: 'center',
-    },
+    padding: 15,
 
-    headingStyle: {
-        fontSize: 30,
+    justifyContent: "center",
+  },
 
-        textAlign: 'center',
+  headingStyle: {
+    fontSize: 30,
 
-        marginBottom: 40,
-    }
+    textAlign: "center",
 
-})
+    marginBottom: 40,
+  },
+});
